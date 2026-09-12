55b68b052361f70d27c95f2b1910dd4ba8c8f793
Rachana Banik <r.banik16@gmail.com>
2026-09-12 12:00:23 -0700

    Add: is-anagram - submission-21

---
+++ Data Structures & Algorithms/is-anagram/submission-21.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> count = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++) {
+
+            char countS = s.charAt(i);
+            char countT = t.charAt(i);
+
+            count.put(countS, count.getOrDefault(countS, 0) +1);
+            count.put(countT, count.getOrDefault(countT, 0) -1);
+
+        }
+
+        for(int j:count.values()) {
+            if(j != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
